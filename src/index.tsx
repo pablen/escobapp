dfea346f32a377a6052230de019bc2081dd2bbc1
Pablo Enrici <pabloenrici@gmail.com>
2026-09-23 00:20:46 -0300

    feat(observability): add Vercel insights and CI artifacts

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import React from 'react'
+import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/react'
 import { registerSW } from 'virtual:pwa-register'
 
 import './index.css'
@@ -9,6 +11,8 @@ import ConfigProvider from './ConfigProvider'
 createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <ConfigProvider />
+    <Analytics />
+    <SpeedInsights />
   </React.StrictMode>,
 )
 
